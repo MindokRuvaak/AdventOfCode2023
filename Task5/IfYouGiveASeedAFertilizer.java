88bb5f5957520b90c58ff0280eeabdd1ecc51db7
MindokRuvaak <cole_daniel@rocketmail.com>
2023-12-15 19:26:14 +0100

    Partk 1 complete

--- Task5/IfYouGiveASeedAFertilizer.java
+++ Task5/IfYouGiveASeedAFertilizer.java
@@ -7,12 +7,15 @@ import java.util.List;
 
 public class IfYouGiveASeedAFertilizer {
     private static final String FILEPATH = "Task5/input.txt";
+    // private static final String FILEPATH = "Task5/controllInput.txt";
+
 
     public static void main(String[] args) {
-        // yerp
         try {
             // System.out.println(Arrays.toString(splitInputLines()));
             Almanac a = new Almanac(splitInputLines());
+            System.out.println(a.findShortestDistance());
+            //MMMM
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -57,6 +60,28 @@ public class IfYouGiveASeedAFertilizer {
             temp2HumidMap = new FarmMap(inputs[6].trim().split("\\n"));
             humid2LocMap = new FarmMap(inputs[7].trim().split("\\n"));
         }
+
+        public long findShortestDistance() {
+            long shortest = Long.MAX_VALUE; //guarantee equal or longer lenght than corresponding seeds
+            for (Long seed : seedList) {
+                long newVal = findLocationForSeed(seed);
+                if (newVal < shortest) {
+                    shortest = newVal;
+                }
+            }
+            return shortest;
+        }
+
+        public long findLocationForSeed(long seed) {
+            long soil = seed2SoilMap.getDestination(seed);
+            long fertelizer = soil2FertMap.getDestination(soil);
+            long water = fert2WaterMap.getDestination(fertelizer);
+            long light = water2LightMap.getDestination(water);
+            long temperature = light2TempMap.getDestination(light);
+            long humidity = temp2HumidMap.getDestination(temperature);
+            long location = humid2LocMap.getDestination(humidity);
+            return location;
+        }
     }
 
     private static class FarmMap {
@@ -72,10 +97,35 @@ public class IfYouGiveASeedAFertilizer {
                     long detinationStart = Long.valueOf(splitRow[0]);
                     long sourceStart = Long.valueOf(splitRow[1]);
                     long length = Long.valueOf(splitRow[2]);
-                    sourceRangeList.add(new long[] { sourceStart, sourceStart + length - 1 });
-                    destinationRangeList.add(new long[] { detinationStart, detinationStart + length - 1 });
+                    // OBS : half open range! the range start is inclusive and range end exclusive
+                    // => [start, start+length)
+                    sourceRangeList.add(new long[] { sourceStart, sourceStart + length});
+                    destinationRangeList.add(new long[] { detinationStart, detinationStart + length});
                 }
             }
         }
+
+        public long getDestination(long source) {
+            for (int i = 0; i < sourceRangeList.size(); i++) {
+                long[] r = sourceRangeList.get(i);
+                if (inRange(r, source)) {
+                    // if source in range "nr" i, then destination in respective range
+                    //distance between source and sourceRangeStart is equal to 
+                    //distance between destination and destinationRangeStart
+                    // => source - sourceRangeStart = x
+                    // && destination = destinationRangeStart + x
+                    // => destination = destinationRangeStart + (source - sourceRangeStart)
+                    return destinationRangeList.get(i)[0] + (source - r[0]);
+                }
+                
+            }
+            // if source is not in any of the provided ranges: source == destination
+            return source;
+        }
+
+    }
+
+    public static boolean inRange(long[] r, long source) {
+        return source >= r[0] && source < r[r.length-1];
     }
 }
